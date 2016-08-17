e9e9ef9ca4e04ae4d5954d0495afaa63518a33fa
Nicolas Bigler <nbigler22@gmail.com>
2016-08-17 21:23:09 +0200

    Refactored so that main is not in a gui class file

--- experimental-data-manager.py
+++ experimental-data-manager.py
@@ -13,42 +13,18 @@ last edited: August 2011
 """
 
 import sys
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui
 
-from src.gui import MainWindow, ParametersDialog
+from src.gui import MainWindowWrap
 
-class ExperimentalDataManager(QtGui.QMainWindow, MainWindow.Ui_MainWindow):
+class ExperimentalDataManager():
     def __init__(self):
-        super(self.__class__, self).__init__()
-        self.setupUi(self)
-        self.actionNew_Entry.setShortcut('Ctrl+N')
-        self.actionNew_Entry.triggered.connect(self.newEntry)
-        self.actionParameters.setShortcut('Ctrl+K')
-        self.actionParameters.triggered.connect(self.showParameters)
-        self.actionQuit.setShortcut('Ctrl+Q')
-        self.actionQuit.triggered.connect(QtGui.qApp.quit)
-        self.searchBar = QtGui.QLineEdit(self)
-        self.toolBar.addWidget(self.searchBar)
-        self.actionSearch.triggered.connect(self.search)
-        
+        self.mainWindow = MainWindowWrap.MainWindowWrap()
+        self.mainWindow.show()
 
-        
-    def newEntry(self):
-        print('new entry')
-
-    def showParameters(self):
-        parameterDialog = ParametersDialog.ParametersDialog(self)
-        parameterDialog.show()
-        
-    def search(self):
-        self.searchBar.setFocus()
-
-        
 def main():
-    
     app = QtGui.QApplication(sys.argv)
-    mainWindow = ExperimentalDataManager()
-    mainWindow.show()
+    appGui = ExperimentalDataManager()
     sys.exit(app.exec_())
 
 
---
+++ src/gui/MainWindowWrap.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt4 tutorial 
+
+This program creates a menubar. The
+menubar has one menu with an exit action.
+
+author: Jan Bodnar
+website: zetcode.com 
+last edited: August 2011
+"""
+
+import sys
+from PyQt4 import QtGui, QtCore
+
+from src.gui import MainWindow, ParametersDialog
+
+class MainWindowWrap(QtGui.QMainWindow, MainWindow.Ui_MainWindow):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.setupUi(self)
+        self.actionNew_Entry.setShortcut('Ctrl+N')
+        self.actionNew_Entry.triggered.connect(self.newEntry)
+        self.actionParameters.setShortcut('Ctrl+K')
+        self.actionParameters.triggered.connect(self.showParameters)
+        self.actionQuit.setShortcut('Ctrl+Q')
+        self.actionQuit.triggered.connect(QtGui.qApp.quit)
+        self.searchBar = QtGui.QLineEdit(self)
+        self.toolBar.addWidget(self.searchBar)
+        self.actionSearch.triggered.connect(self.search)
+        
+
+        
+    def newEntry(self):
+        print('new entry')
+
+    def showParameters(self):
+        parameterDialog = ParametersDialog.ParametersDialog(self)
+        parameterDialog.show()
+        
+    def search(self):
+        self.searchBar.setFocus()
+
+        
